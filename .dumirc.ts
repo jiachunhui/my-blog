1b90b97209e41f9b86a3fe4fda921bea41922ff3
jiachunhui <306727208@qq.com>
2023-10-13 16:24:00 +0800

    2

--- .dumirc.ts
+++ .dumirc.ts
@@ -4,7 +4,7 @@ export default defineConfig({
   outputPath: 'dist',
   themeConfig: {
     name: 'blog',
-    nav:[{ title: '学习笔记', link: '/guide' }],
+    nav:[{ title: '学习笔记', link: '/guide' },{ title: '收藏', link: '/favorites' }],
     showLineNum:true,
     footer:'技术支持 <a href="https://www.ccxcn.com">传诚信.网站建设</a>',
     socialLinks:{
---
+++ docs/Favorites/index.md
@@ -0,0 +1,14 @@
+---
+group:
+  title: 收藏夹
+  order: -1
+title: Vercel
+order: 0
+---
+
+* [Vercel]() #免费提供运行环境，神一样的存在
+> [自己AI平台](https://gpt2.wzjs100.com/) \ [自己博客](https://blog.aiyy.site)
+
+* [StackBlitz](https://stackblitz.com/) #在线运行编辑代码的平台
+
+
